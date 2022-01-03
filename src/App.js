5f203a4b1d5c7f42274d59a2495ecb28723d0823
Satyam Jaiswal <satyam.jaiswal26@gmail.com>
2022-01-03 11:02:20 +0530

    🔥 Good to go

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-import { ThemeProvider } from 'styled-components';
-import theme from './theme';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HeartOutlined } from "@ant-design/icons";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
 
 // Components
-import Home from './components/Home';
-import CreateResume from './components/createResume';
+import Home from "./components/Home";
+import CreateResume from "./components/createResume";
 
 const App = () => {
-	return (
-		<ThemeProvider theme={theme}>
-			<Router>
-				<Switch>
-					<Route path="/create" component={CreateResume} />
-					<Route exact path="/" component={Home} />
-				</Switch>
-			</Router>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/create" component={CreateResume} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
+      <strong style={{ float: "left", marginLeft: "10px" }}>
+        Resume Builder
+      </strong>
+      <p style={{ float: "right", marginRight: "10px" }}>
+        Made with <HeartOutlined /> by Satyam Jaiswal
+      </p>
+    </ThemeProvider>
+  );
 };
 
 export default App;
